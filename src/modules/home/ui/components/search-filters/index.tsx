f1bb4743158204ea97e5096d138bd527207d6096
Tariq-act <khantarikact@gmail.com>
2025-06-05 01:23:27 +0530

    32: search filters fix

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -6,6 +6,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
 
+import { useProductFilters } from "@/modules/products/hooks/use-product-filters";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
 import { BreadcrumbNavigation } from "./breadcrumb-navigation";
@@ -13,6 +14,8 @@ import { BreadcrumbNavigation } from "./breadcrumb-navigation";
 export const SearchFilters = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+
+  const [filters, setFilters] = useProductFilters();
 
   const params = useParams();
   const categoryParams = params.category as string | undefined;
@@ -36,7 +39,10 @@ export const SearchFilters = () => {
       className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
       style={{ backgroundColor: activeCategoryColor }}
     >
-      <SearchInput />
+      <SearchInput
+        defaultValue={filters.search}
+        onChange={(value) => setFilters({ search: value })}
+      />
       <div className="hidden lg:block">
         <Categories data={data} />
       </div>
